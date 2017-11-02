6f24c5b25ff9a0cae1c89879a046b5d1d0c105fd
Mayank Agarwal <mayank.agarwal0312@gmail.com>
2017-11-01 20:02:24 -0400

    Spatial BN done

--- Assignments/assignment2/asgn2/.ipynb_checkpoints/layers-checkpoint.py
+++ Assignments/assignment2/asgn2/.ipynb_checkpoints/layers-checkpoint.py
@@ -646,7 +646,11 @@ def spatial_batchnorm_forward(x, gamma, beta, bn_param):
   # version of batch normalization defined above. Your implementation should  #
   # be very short; ours is less than five lines.                              #
   #############################################################################
-  pass
+  N, C, H, W = x.shape
+  x = x.transpose(0, 2, 1, 3).transpose(0, 1, 3, 2).reshape(N*H*W, C)
+  x_bn, bn_cache = batchnorm_forward(x, gamma, beta, bn_param)
+  out = x_bn.reshape(N, H, W, C).transpose(0, 3, 2, 1).transpose(0, 1, 3, 2)
+  cache = bn_cache
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
@@ -676,7 +680,10 @@ def spatial_batchnorm_backward(dout, cache):
   # version of batch normalization defined above. Your implementation should  #
   # be very short; ours is less than five lines.                              #
   #############################################################################
-  pass
+  N, C, H, W = dout.shape
+  dout = dout.transpose(0, 2, 1, 3).transpose(0, 1, 3, 2).reshape(N*H*W, C)
+  dx, dgamma, dbeta = batchnorm_backward(dout, cache)
+  dx = dx.reshape(N, H, W, C).transpose(0, 3, 2, 1).transpose(0, 1, 3, 2)
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
